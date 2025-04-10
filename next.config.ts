4bdca876fb591dde9dbffafcbda02bb5ef9094f0
Hida <me@wanghaida.com>
2025-04-10 10:39:15 +0800

    feat: 开启 LFS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    output: 'export',
+    output: 'standalone',
     transpilePackages: ['three'],
     typescript: {
         ignoreBuildErrors: true,
