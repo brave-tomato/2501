ad4dda0903600a35eaacbb78201e1a82312d23de
Hida <hida1024.w@gmail.com>
2025-06-19 20:58:26 +0800

    feat: logo

--- src/app/[locale]/(fullpage)/solution/a1/page.tsx
+++ src/app/[locale]/(fullpage)/solution/a1/page.tsx
@@ -33,16 +33,6 @@ const sections = [
         ],
         image: '/static/solution/a1/01.jpg',
         link: '/static/solution/a1/01.pdf',
-        logo: [
-            {
-                image: '/static/solution/a1/01.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/02.jpg',
-                link: 'https://baidu.com',
-            },
-        ],
         title: '纯电乘用车解决方案',
     },
     {
@@ -54,32 +44,6 @@ const sections = [
         ],
         image: '/static/solution/a1/02.jpg',
         link: '/static/solution/a1/02.pdf',
-        logo: [
-            {
-                image: '/static/solution/a1/01.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/02.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/03.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/04.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/05.jpg',
-                link: 'https://baidu.com',
-            },
-            {
-                image: '/static/solution/a1/06.jpg',
-                link: 'https://baidu.com',
-            },
-        ],
         title: '电动快艇解决方案',
     },
     {
@@ -117,6 +81,29 @@ const sections = [
         ],
         image: '/static/solution/a1/05.jpg',
         link: '/static/solution/a1/05.pdf',
+        logo: [
+            { image: '/static/solution/a1/logo/01.jpg' },
+            { image: '/static/solution/a1/logo/02.jpg' },
+            { image: '/static/solution/a1/logo/03.jpg' },
+            { image: '/static/solution/a1/logo/04.jpg' },
+            { image: '/static/solution/a1/logo/05.jpg' },
+            { image: '/static/solution/a1/logo/06.jpg' },
+            { image: '/static/solution/a1/logo/07.jpg' },
+            { image: '/static/solution/a1/logo/08.jpg' },
+            { image: '/static/solution/a1/logo/09.jpg' },
+            { image: '/static/solution/a1/logo/10.jpg' },
+            { image: '/static/solution/a1/logo/11.jpg' },
+            { image: '/static/solution/a1/logo/12.jpg' },
+            { image: '/static/solution/a1/logo/13.jpg' },
+            { image: '/static/solution/a1/logo/14.jpg' },
+            { image: '/static/solution/a1/logo/15.jpg' },
+            { image: '/static/solution/a1/logo/16.jpg' },
+            { image: '/static/solution/a1/logo/17.jpg' },
+            { image: '/static/solution/a1/logo/18.jpg' },
+            { image: '/static/solution/a1/logo/19.jpg' },
+            { image: '/static/solution/a1/logo/20.jpg' },
+            { image: '/static/solution/a1/logo/21.jpg' },
+        ],
         title: '电动摩托车解决方案',
     },
     {
@@ -212,7 +199,19 @@ export default () => {
                                                 <Swiper loop={true} slidesPerView={5} style={{ marginTop: 80 }}>
                                                     {section.logo.map((logo, index) => (
                                                         <SwiperSlide key={index} style={{ textAlign: 'center' }}>
-                                                            <Link href={logo.link} target="_blank">
+                                                            {logo?.link ? (
+                                                                <Link href={logo.link} target="_blank">
+                                                                    <img
+                                                                        alt=""
+                                                                        src={logo.image}
+                                                                        style={{
+                                                                            width: 79,
+                                                                            height: 36,
+                                                                            objectFit: 'contain',
+                                                                        }}
+                                                                    />
+                                                                </Link>
+                                                            ) : (
                                                                 <img
                                                                     alt=""
                                                                     src={logo.image}
@@ -222,7 +221,7 @@ export default () => {
                                                                         objectFit: 'contain',
                                                                     }}
                                                                 />
-                                                            </Link>
+                                                            )}
                                                         </SwiperSlide>
                                                     ))}
                                                 </Swiper>
