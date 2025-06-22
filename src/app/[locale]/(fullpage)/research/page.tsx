fc35fd9411edd4f3717e8df4abad83e753602a04
Hida <hida1024.w@gmail.com>
2025-06-22 16:00:39 +0800

    feat: image

--- src/app/[locale]/(fullpage)/research/page.tsx
+++ src/app/[locale]/(fullpage)/research/page.tsx
@@ -348,6 +348,20 @@ export default () => {
                                                             cursor: 'pointer',
                                                         }}
                                                     />
+
+                                                    <img
+                                                        alt=""
+                                                        src="/static/research/play.png"
+                                                        style={{
+                                                            position: 'absolute',
+                                                            top: '50%',
+                                                            left: '50%',
+                                                            transform: 'translate(-50%, -50%)',
+                                                            width: 100,
+                                                            height: 100,
+                                                            cursor: 'pointer',
+                                                        }}
+                                                    />
                                                 </AspectRatio>
 
                                                 <div className={styles.technologySwiperContent}>
